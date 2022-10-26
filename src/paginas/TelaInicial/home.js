fef9116fa8d6b35fbc5eaabc19eb8a1e477e5d51
Pedro Reis <phbarcelosreis66@gmail.com>
2022-10-26 16:58:35 -0300

    layout hoje 20%

--- src/paginas/TelaInicial/home.js
+++ src/paginas/TelaInicial/home.js
@@ -1,7 +1,49 @@
+import styled from "styled-components"
+
+const Page = styled.div`
+    width: 375px;
+    height: 667px;
+    background: #0E0E13;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+`
+
+const Button = styled.a`
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 18px 122px;
+    gap: 10px;
+    width: 299px;
+    height: 52px;
+    background: #FF4791;
+    border-radius: 8px;
+    cursor: pointer;
+    text-decoration: none;
+    & p{
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+        color: #FFFFFF;
+    }
+`
+
+
 function TelaInicial(){
 
     return(
-        <div>oi</div>
+        <Page>
+            <Button href="https://www.youtube.com/" target="_blank"><p>Teste</p></Button>
+            <Button href="https://www.twitter.com/" target="_blank"><p>TESTES</p></Button>
+
+        </Page>
 
     )
 
